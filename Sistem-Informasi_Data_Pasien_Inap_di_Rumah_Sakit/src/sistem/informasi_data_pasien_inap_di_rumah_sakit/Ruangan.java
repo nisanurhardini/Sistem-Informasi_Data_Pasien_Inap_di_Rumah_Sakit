f698adc83d57036fb0e0ea3399c826ebefeb3f53
KukuhSanddi <kukuhsanddi12@gmail.com>
2017-02-24 19:01:19 +0700

    Add Class

---
+++ Sistem-Informasi_Data_Pasien_Inap_di_Rumah_Sakit/src/sistem/informasi_data_pasien_inap_di_rumah_sakit/Ruangan.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistem.informasi_data_pasien_inap_di_rumah_sakit;
+
+/**
+ *
+ * @author Kukuh Sanddi
+ */
+public class Ruangan {
+    
+}
